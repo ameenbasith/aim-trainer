a7d59fd7a8e5a981c245c104f4bbcd4a73fe0f05
Ameen <ameenbasith2000@gmail.com>
2023-10-29 21:29:47 -0700

    import statements w initialization

---
+++ main.py
@@ -0,0 +1,29 @@
+import math
+import random
+import time
+import pygame
+
+pygame.init()
+
+WIDTH, HEIGHT = 800, 600
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Aim Trainer")  # name of the window
+
+
+#  we need an infinite loop while we run the game
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
