7edd1629ee9a2efad50ca00812161c559e882080
Ameen <ameenbasith2000@gmail.com>
2023-11-01 13:56:07 -0700

    game analytics

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ TARGET_EVENT = pygame.USEREVENT
 TARGET_PADDING = 30
 
 BG_COLOR = (0, 25, 40)
+LIVES = 3
+TOP_BAR_HEIGHT = 50
+
+LABEL_FONT = pygame.font.SysFont("comicsans", 24)
 
 
 class Target:
@@ -49,14 +53,23 @@ class Target:
         dis = math.sqrt((self.x - x)**2 + (self.y - y)**2)
         return dis <= self.size
 
-
+def format_time(secs):
+    milli = math.floor(int(secs * 1000 % 1000) / 100)
+    seconds = int(round(secs % 60, 1))
+    minutes = int(secs // 60)
+    return f"{minutes:02d}:{seconds:02d}.{milli}"
 def draw(win, targets):  # need to clear the screen every single time we draw
     win.fill(BG_COLOR)
 
     for target in targets:
         target.draw(win)
 
-    pygame.display.update()
+def draw_top_bar(win, elapsed_time, targets_pressed, misses):
+    pygame.draw.rect(win, "grey", (0, 0, WIDTH, TOP_BAR_HEIGHT))
+    time_label = LABEL_FONT.render(f"Time: {format_time(elapsed_time)}", 1, "black")
+
+    win.blit(time_label, (5, 5))
+
 
 
 #  we need an infinite loop while we run the game
@@ -76,6 +89,7 @@ def main():
         clock.tick(244)  # regulates the speed at which the while loop runs
         click = False
         mouse_pos = pygame.mouse.get_pos()
+        elapsed_time = time.time() - start_time
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -103,7 +117,12 @@ def main():
                 targets.remove(target)
                 target_pressed += 1
 
+        if misses >= LIVES:
+            pass  # game ends
+
         draw(WIN, targets)
+        draw_top_bar(WIN, elapsed_time, target_pressed, misses)
+        pygame.display.update()
 
     pygame.quit()
 
